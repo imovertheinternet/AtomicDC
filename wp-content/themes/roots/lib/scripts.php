30611578df6949c4227b077e0f893fa97a63ec3a
lukevinskywynn <lukevinskywynn@gmail.com>
2014-01-30 15:21:20 -0600

    background color change on quote

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a81936f085fb69584dc2ae807991d547');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2180cc47b54b397ca62781a0ce4aa209');
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/animate.min.css');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
