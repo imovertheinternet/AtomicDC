eb3d7371183acc1e3d85c350d48ba1892bcd6ff2
Conrad <imovertheinternet@gmail.com>
2014-01-31 17:08:04 -0600

    worked on the footer. lookin gooooood

--- wp-content/themes/roots/templates/footer.php
+++ wp-content/themes/roots/templates/footer.php
@@ -1,9 +1,62 @@
+<div class="col-lg-12" style="background-color:#283038;color:#fff;padding-top:15px;padding-bottom:15px;font-family:'Domine',serif;font-size:12px;">
+<div class="container">
+<div class="col-sm-4 col-md-4">
+<h4>Twitter</h4>
+<p>Over ten years driving business to our clients with "Ah-ha" marketing moments, prospect point-of-need discovery, and comprehensive digital marketing strategies that involve web design, email marketing, search engine oprtimization, and social marketing. 
+</p>
+Request an Internet Marketing Audit<br>
+<br>Increase leads. Call us today: 972-668-3867
+<br><p>Plano web design office: 
+5717 Legacy Dr. Suite 290 Plano, TX 75024</p>
+</div>
+
+<div class="col-sm-3 col-md-3">
+<H4>Web Design Services</H4>
+<ul class="iconlistfooter">
+<i class="icon-list fa fa-angle-double-right fa-lg"></i> Web Design & Development<br><br>
+<i class="icon-list fa fa-angle-double-right fa-lg"></i> Email Marketing<br><br>
+<i class="icon-list fa fa-angle-double-right fa-lg"></i> SEO /SEM<br><br>
+<i class="icon-list fa fa-angle-double-right fa-lg"></i> Social Marketing<br><br>
+<i class="icon-list fa fa-angle-double-right fa-lg"></i> Website Maintenance<br><br>
+</ul>
+</div>
+
+<div class="col-sm-2 col-md-2">
+<H4>Elsewhere</H4>
+<ul class="iconlistfooter">
+<i class="icon-list fa fa-facebook fa-2x"></i>Facebook<br><br>
+<i class="icon-list fa fa-twitter fa-2x"></i> Twitter<br><br>
+<i class="icon-list fa fa-google-plus fa-2x"></i> Google+<br><br>
+<i class="icon-list fa fa-instagram fa-2x"></i> Instagram<br><br>
+</ul>
+</div>
+
+<div class="col-sm-3 col-md-3">
+<h4>Email Updates</h4>
+Get emails about new theme launches & future updates.
+<br>
+<form role="form">
+  <div class="form-group">
+    <label for="exampleInputEmail1"></label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-12" style="background-color:#1d2429;border-top:1px dashed #37424d">
+<div class="container" style="color:white;padding-top:10px;font-size:12px;">
+<?php dynamic_sidebar('sidebar-footer'); ?>
+      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name');?>
+</div>
+</div>
+
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    </div>
+    
   </div>
 </footer>
 
