cb24d5306aeaefe3d115cb1b57f12c549981cd1c
lukevinskywynn <lukevinskywynn@gmail.com>
2014-01-31 14:49:31 -0600

    container fix to allow fluid width on items
    also responsive fix for text in featured image

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd2a88ed7904bb12859d65a751206c62d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd4e7624d5e944c8f98297b42b344c5c3');
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/animate.min.css');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
