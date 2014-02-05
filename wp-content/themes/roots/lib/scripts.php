126f58a77e2b45115558c300eb49743f330f2218
lukevinskywynn <lukevinskywynn@gmail.com>
2014-02-05 09:23:48 -0600

    added galaga game easter egg

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd4e7624d5e944c8f98297b42b344c5c3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7cf8f5cb6588ac512546472e8f36c1d9');
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/animate.min.css');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
---
+++ wp-content/themes/roots/templates/galaga-game.php
@@ -0,0 +1,101 @@
+<script type="text/javascript" src="/atomic/wp-content/themes/roots/galaga/js/boss_weapon.js"></script>
+<script type="text/javascript" src="/atomic/wp-content/themes/roots/galaga/js/galaga.js"></script>
+
+<div class="container galaga-game">
+	<div class="col-md-8">
+		<canvas id="galaga_canvas" width="400" height="400" style="background-color:black;"></canvas>
+	</div>
+	<div class="col-md-4 galaga-info">
+		<h1>Key Buttons</h1>
+		<p>
+			<b>Arrow Key</b> : Move <br>
+			"<b>space</b>" : Bullet <br>
+			"<b>z</b>" : Bomb <br>
+			"<b>x</b>" : Shutgun <br>
+			"<b>c</b>" : Shild <br>
+			"<b>shift</b>" + "<b>arrow</b>" : Homming a.k.a 'torpedo number 1' of North Korea. <br>
+		</p>
+	</div>
+</div>
+
+<div id="img_source" style="display:none;">
+	<img id="bad1" src="/atomic/wp-content/themes/roots/galaga/img/bad2.png">
+	<img id="bad2" src="/atomic/wp-content/themes/roots/galaga/img/bad3.png">
+	<img id="bad3" src="/atomic/wp-content/themes/roots/galaga/img/bad1.png">
+	<img id="good" src="/atomic/wp-content/themes/roots/galaga/img/good.png">
+	<img id="suri" src="/atomic/wp-content/themes/roots/galaga/img/suri.png">
+	<img id="vim" src="/atomic/wp-content/themes/roots/galaga/img/vim.png">
+	<img id="laser" src="/atomic/wp-content/themes/roots/galaga/img/laser1" width="40" height="24">
+	<img id="boss" src="/atomic/wp-content/themes/roots/galaga/img/bc.png" width="90" height="70">
+	<img id="explosion" src="/atomic/wp-content/themes/roots/galaga/img/explosion1.png">
+</div>
+<audio id="sound0">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga0.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga0.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound1">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga1.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga1.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound2">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga2.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga2.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound3">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga3.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga3.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound4">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga4.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga4.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound5">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga5.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga5.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound6">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga6.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga6.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound7">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga7.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga7.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound8">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga8.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga8.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound9">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga9.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga9.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound11">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga11.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga11.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound12">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga12.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga12.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound13">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga13.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga13.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
+<audio id="sound14">
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga14.mp3"></source>
+    <source src="/atomic/wp-content/themes/roots/galaga/media/galaga14.wav"></source>
+    Your browser isn't invited for super fun audio time.
+</audio>
