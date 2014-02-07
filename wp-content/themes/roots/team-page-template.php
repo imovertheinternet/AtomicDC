5eb9a98b8089dd1ecb5e3031ad13bc1586c23813
lukevinskywynn <lukevinskywynn@gmail.com>
2014-02-07 17:02:22 -0600

    team page build and animated

---
+++ wp-content/themes/roots/team-page-template.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Template Name: Team Page
+*/
+?>
+
+<div class="container team-sandbox">
+	<div class="hexagon hexagon1">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-2-first">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-2">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-2">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-2">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-2">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-3-first">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-3">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+	<div class="hexagon hexagon1 row-3-last">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2">
+				<a href="http://localhost/atomic/?page_id=133">
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
