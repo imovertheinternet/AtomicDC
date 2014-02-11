e05a94e166094356e7a4ea0338802c94415fdfef
lukevinskywynn <lukevinskywynn@gmail.com>
2014-02-11 12:41:21 -0600

    individual post pages - some fixes on team page

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cd5688f7cd74e987d3a2d653712dc8bd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e0acf0a815c6250d946124e0515b6e76');
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/animate.min.css');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
